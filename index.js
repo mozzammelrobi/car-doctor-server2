5c1b5125c144ed582ebb85c10848d15c23d26eb9
mozzammelrobi <mozzammelfb@gmail.com>
2024-08-14 22:56:33 +0600

    jwt or token generate and send to client and again sent cookie to server

--- index.js
+++ index.js
@@ -1,13 +1,19 @@
 const express = require('express')
 const cors = require('cors')
+const jwt = require('jsonwebtoken')
+const cookieParser= require('cookie-parser')
 require('dotenv').config()
 const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 const app = express();
 const port = process.env.PORT || 5000;
 
 // middleware
-app.use(cors())
-app.use(express.json())
+app.use(cors({
+  origin:['http://localhost:5173'],
+  credentials:true
+}))
+app.use(express.json());
+app.use(cookieParser())
 
 
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.mrpyxhs.mongodb.net/?appName=Cluster0`;
@@ -26,6 +32,21 @@ async function run() {
     const serviceCollection = client.db('carDoctor').collection('services')
     const bookingCollection = client.db('carDoctor').collection('bookings')
 
+    // auth related api 
+    app.post('/jwt', async (req, res) => {
+      const user = req.body;
+      console.log(user)
+      const token = jwt.sign(user, process.env.ACCESS_TOKEN_SECRET, { expiresIn: '1h' })
+      res
+        .cookie('token', token, {
+          httpOnly: true,
+          secure: false,
+          sameSite: 'strict'
+        })
+        .send({success:true})
+    })
+
+    // service related api
     app.get('/services', async (req, res) => {
       const cursor = serviceCollection.find()
       const result = await cursor.toArray()
@@ -52,6 +73,8 @@ async function run() {
     // booking / checkout l
     app.get('/bookings', async (req, res) => {
       // console.log(req.query.email)
+      // console.log('tokeeeeen',req.cookies.token)
+      console.log('Cookie Token:', req.cookies.token);
       let query = {}
       if (req.query?.email) {
         query = { email: req.query.email }
